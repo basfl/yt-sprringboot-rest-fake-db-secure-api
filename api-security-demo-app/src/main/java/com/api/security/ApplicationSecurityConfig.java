5e7fc2e83fec94a54b27f97b886e9de2136c4887
unknown <jupiter85838@yahoo.com>
2021-03-14 22:43:29 -0400

    adding redirect

--- api-security-demo-app/src/main/java/com/api/security/ApplicationSecurityConfig.java
+++ api-security-demo-app/src/main/java/com/api/security/ApplicationSecurityConfig.java
@@ -31,31 +31,27 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		// super.configure(http);
-		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-		.and()
-		.authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*").permitAll().antMatchers("/api/**")
-				.hasAnyRole(STUDENT.name()).anyRequest().authenticated().and().httpBasic();
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().authorizeRequests()
+				.antMatchers("/", "index", "/css/*", "/js/*").permitAll().antMatchers("/api/**")
+				.hasAnyRole(STUDENT.name()).anyRequest().authenticated()
+				.and().formLogin().loginPage("/login")
+				.permitAll().defaultSuccessUrl("/courses",true);
 
 	}
 
 	@Override
 	@Bean
 	public UserDetailsService userDetailsService() {
-		UserDetails user1 = User.builder().username("user1").password(passwordEncoder.encode("user1@123"))
-			//	.roles(STUDENT.name()) // ROLE_STUDENT
-				.authorities(STUDENT.getGrantedAuthorities())
-				.build();
+		UserDetails user1 = User.builder().username("user1").password(passwordEncoder.encode("123"))
+				// .roles(STUDENT.name()) // ROLE_STUDENT
+				.authorities(STUDENT.getGrantedAuthorities()).build();
 		UserDetails user2 = User.builder().username("user2").password(passwordEncoder.encode("user2@123"))
-			//	.roles(ADMIN.name()) // ROLE_ADMIn
-				.authorities(ADMIN.getGrantedAuthorities())
-				.build();
-		 UserDetails user3 = User.builder()
-	                .username("user3")
-	                .password(passwordEncoder.encode("user3@123"))
+				// .roles(ADMIN.name()) // ROLE_ADMIn
+				.authorities(ADMIN.getGrantedAuthorities()).build();
+		UserDetails user3 = User.builder().username("user3").password(passwordEncoder.encode("user3@123"))
 //	                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
-	                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-	                .build();
-		return new InMemoryUserDetailsManager(user1, user2,user3);
+				.authorities(ADMINTRAINEE.getGrantedAuthorities()).build();
+		return new InMemoryUserDetailsManager(user1, user2, user3);
 	}
 
 }
