5da84ecf1bb7f720c7d871f846f0cdffd9c8577c
unknown <jupiter85838@yahoo.com>
2021-03-18 13:36:56 -0400

    add jwt filter

---
+++ api-security-demo-app/src/main/java/com/api/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.api.jwt;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	private final AuthenticationManager authenticationManager;
+
+	public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+
+	}
+
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		try {
+			UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
+					.readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+			Authentication authentication = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+					authenticationRequest.getPassword());
+
+			Authentication authenticate = authenticationManager.authenticate(authentication);
+			return authenticate;
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		Key key = Keys.hmacShaKeyFor("secretsecrte".getBytes());
+
+		String token = Jwts.builder().setSubject(authResult.getName()).claim("authorities", authResult.getAuthorities())
+				.setIssuedAt(new java.util.Date()).setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+				.signWith(key).compact();
+
+		response.addHeader("Authorization", "Bearer " + token);
+
+	}
+
+}
